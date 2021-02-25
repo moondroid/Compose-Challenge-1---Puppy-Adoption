2456f2cce4b13e872984de51b1d427c843aa8472
Marco Granatiero <marco.granatiero@assistdigital.com>
2021-02-26 00:34:48 +0100

    add image loading with coil

--- app/src/main/java/com/example/androiddevchallenge/ui/detail/DetailScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/detail/DetailScreen.kt
@@ -1,20 +1,30 @@
 package com.example.androiddevchallenge.ui.detail
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.R
 import com.example.androiddevchallenge.data.INITIAL_PUPPIES
 import com.example.androiddevchallenge.model.Puppy
+import com.example.androiddevchallenge.ui.theme.typography
+import dev.chrisbanes.accompanist.coil.CoilImage
 
 @Composable
 fun DetailScreen(
@@ -37,14 +47,46 @@ fun DetailScreen(
 fun PuppyDetail(
     puppy: Puppy
 ) {
+    Column(
+        modifier = Modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CoilImage(
+            data = "https://loremflickr.com/320/240/puppy?lock=${puppy.id}",
+            contentDescription = puppy.name,
+            modifier = Modifier
+                .height(180.dp)
+                .fillMaxWidth()
+                .clip(shape = RoundedCornerShape(4.dp)),
+            contentScale = ContentScale.Crop,
+            loading = {
+                Box(Modifier.matchParentSize()) {
+                    CircularProgressIndicator(Modifier.align(Alignment.Center))
+                }
+            },
+            fadeIn = true
+        )
+        Spacer(Modifier.height(16.dp))
 
-    Column(modifier = Modifier.padding(16.dp), horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = puppy.name, style = MaterialTheme.typography.h4)
+        Text(
+            puppy.name,
+            style = typography.h6,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(
+            "Davenport, California",
+            style = typography.body2
+        )
+        Text(
+            "December 2018",
+            style = typography.body2
+        )
     }
 }
 
 @Preview("Detail Screen", widthDp = 360, heightDp = 640)
 @Composable
-fun DetailScreenPreview(){
+fun DetailScreenPreview() {
     DetailScreen(0)
 }
--- app/src/main/java/com/example/androiddevchallenge/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/home/HomeScreen.kt
@@ -1,22 +1,29 @@
 package com.example.androiddevchallenge.ui.home
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Divider
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.R
 import com.example.androiddevchallenge.model.Puppy
+import dev.chrisbanes.accompanist.coil.CoilImage
 
 @Composable
 fun HomeScreen(
@@ -46,7 +53,7 @@ fun PuppyList(
 
         items(items = puppies) { puppy ->
             PuppyItem(puppy = puppy, onPuppyClicked = onPuppyClicked)
-            Divider(color = Color.Gray)
+            // Divider(color = Color.Gray)
         }
     }
 }
@@ -56,16 +63,37 @@ fun PuppyItem(
     puppy: Puppy,
     onPuppyClicked: (Int) -> Unit
 ) {
-    Text(text = puppy.name,
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
             .fillMaxWidth()
             .clickable { onPuppyClicked(puppy.id) }
-            .padding(16.dp))
+            .padding(8.dp)
+    ) {
+        CoilImage(
+            data = "https://loremflickr.com/320/240/puppy?random=${puppy.id}&lock=${puppy.id}",
+            contentDescription = puppy.name,
+            modifier = Modifier
+                .size(50.dp)
+                .clip(shape = RoundedCornerShape(8.dp))
+        )
+        Spacer(Modifier.width(16.dp))
+        Text(
+            text = puppy.name,
+            style = MaterialTheme.typography.subtitle1
+        )
+    }
 }
 
+@Preview("Puppy Item", widthDp = 360, heightDp = 640)
+@Composable
+fun PuppyItemPreview(){
+    PuppyItem(Puppy(0, "Cat"), {})
+}
 
 @Preview("Home Screen", widthDp = 360, heightDp = 640)
 @Composable
 fun HomeScreenPreview(){
     HomeScreen(puppies = listOf(Puppy(0, "Cat"), Puppy(1, "Dog")), {})
 }
+
